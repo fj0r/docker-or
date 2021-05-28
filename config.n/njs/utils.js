c2c3c11d06f2d34ba8e3b67fd8e6b574e004b22e
nash <nash@iffy.me>
2021-05-28 14:01:30 +0800

    utils.js

--- config.n/njs/utils.js
+++ config.n/njs/utils.js
@@ -1,3 +1,5 @@
+const fs = require('fs').promises
+
 function main(r) {
     let u = r.uri.split('/').filter(x=>x.length > 0)
     if ( u.length > 1 && METHODS[u[1]] ) {
@@ -12,9 +14,11 @@ const METHODS = {
     ip      : r => r.return(200, r.remoteAddress),
     rdr     : r => r.return(301, r.args.url),
     ua      : r => r.return(200, r.headersIn['User-Agent']),
+    body    : r => r.return(200, r.requestBuffer),
+    version : r => r.return(200, JSON.stringify({ngx: r.variables.nginx_version, njs: njs.version, tz: process.env.TIMEZONE})),
+    errorLog: r => fs.readFile('/opt/nginx/logs/error.log').then(data=>r.return(200, data)),
 }
 
-const body = () => {}
 const run = () => {}
 
 export default { main }
